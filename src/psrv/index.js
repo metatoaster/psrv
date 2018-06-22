712c1282ae87edb8591aea6d3aa369f01b3815b0
Tommy Yu <y@metatoaster.com>
2018-06-22 16:27:10 +0100

    Implement a lazy client side polling update.
    - Update the CPU of the host on the fly on the client by having it poll
      the end point periodically.

---
+++ src/psrv/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gaugesrv = require('gaugesrv/lib');
+var $ = require('jquery');
+var interval = 3000; // milliseconds
+
+
+function update() {
+    $.get('/status', function(data) {
+        console.log(data);
+        $.each(data, gaugesrv.toggle);
+        setTimeout(update, interval);
+    });
+};
+
+
+function main() {
+    var gauges = $('canvas[data-psrv]');
+    if (gauges.length == 0) {
+        // don't set up the polling
+        return;
+    }
+    update();
+}
+
+
+window.document.addEventListener('DOMContentLoaded', main);
